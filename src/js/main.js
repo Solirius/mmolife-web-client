f559c3cf356ebb62db6cc7243b9654f6cea626c3
Linus Norton <linusnorton@gmail.com>
2019-11-08 08:43:53 +0000

    basic grid

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,45 @@ var packet = {
     ]
 };
 var Game = /** @class */ (function () {
-    function Game(canvasEl) {
+    function Game(canvasEl, grid) {
         this.canvasEl = canvasEl;
+        this.grid = grid;
     }
     Game.prototype.render = function () {
+        // todo does this need to happen every tick
+        // todo fps counter
         var ctx = this.canvasEl.getContext("2d");
-        ctx.moveTo(0, 0);
-        ctx.lineTo(200, 100);
-        ctx.stroke();
+        this.grid.render(ctx);
+    };
+    Game.prototype.init = function () {
+        this.canvasEl.width = window.innerWidth - 50;
+        this.canvasEl.height = window.innerHeight - 20;
     };
     return Game;
 }());
-var game = new Game(document.getElementById("game"));
+var Grid = /** @class */ (function () {
+    function Grid(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Grid.prototype.render = function (ctx) {
+        var cellWidth = ctx.canvas.width / this.width;
+        for (var i = 0; i < this.width; i++) {
+            var x = i * cellWidth;
+            ctx.moveTo(Math.floor(x), 0);
+            ctx.lineTo(Math.floor(x), ctx.canvas.height);
+            ctx.stroke();
+        }
+        var cellHeight = ctx.canvas.height / this.height;
+        for (var i = 0; i < this.height; i++) {
+            var y = i * cellHeight;
+            ctx.moveTo(0, Math.floor(y));
+            ctx.lineTo(ctx.canvas.width, Math.floor(y));
+            ctx.stroke();
+        }
+    };
+    return Grid;
+}());
+var game = new Game(document.getElementById("game"), new Grid(40, 20));
+game.init();
 requestAnimationFrame(game.render.bind(game));
--- src/js/main.ts
+++ src/js/main.ts
@@ -15,18 +15,58 @@ const packet = {
 class Game {
 
   constructor(
-    public readonly canvasEl: HTMLCanvasElement
+    private readonly canvasEl: HTMLCanvasElement,
+    private readonly grid: Grid,
   ) {}
 
-  public render() {
-    const ctx = this.canvasEl.getContext("2d")!;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
+  public render(): void {
+    // todo does this need to happen every tick
+    // todo fps counter
+    const ctx: CanvasRenderingContext2D = this.canvasEl.getContext("2d")!;
+
+    this.grid.render(ctx);
+  }
+
+  public init(): void {
+    this.canvasEl.width = window.innerWidth - 50;
+    this.canvasEl.height = window.innerHeight - 20;
   }
 
 }
 
-const game = new Game(document.getElementById("game") as HTMLCanvasElement);
+class Grid {
+  constructor(
+    private readonly width: number,
+    private readonly height: number
+  ) {}
+
+  public render(ctx: CanvasRenderingContext2D): void {
+    const cellWidth = ctx.canvas.width / this.width;
+
+    for (let i = 0; i < this.width; i++) {
+      const x = i * cellWidth;
+      ctx.moveTo(Math.floor(x), 0);
+      ctx.lineTo(Math.floor(x), ctx.canvas.height);
+      ctx.stroke();
+    }
+
+    const cellHeight = ctx.canvas.height / this.height;
+
+    for (let i = 0; i < this.height; i++) {
+      const y = i * cellHeight;
+      ctx.moveTo(0, Math.floor(y));
+      ctx.lineTo(ctx.canvas.width, Math.floor(y));
+      ctx.stroke();
+    }
+
+  }
+}
+
+const game = new Game(
+  document.getElementById("game") as HTMLCanvasElement,
+  new Grid(40, 20)
+);
+
+game.init();
 
 requestAnimationFrame(game.render.bind(game));
