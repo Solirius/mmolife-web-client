f5e1c7c0e9c7e462751a10b15496501d545f20e2
Linus Norton <linusnorton@gmail.com>
2019-11-08 10:00:22 +0000

    Process the packet

--- src/js/main.js
+++ src/js/main.js
@@ -4,25 +4,46 @@ var packet = {
         "Jose": "00ffee"
     },
     grid: [
-        ["Bob", null, null, null, null, null],
-        [null, null, "Jose", null, null, null],
-        [null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+        ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+        [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
     ]
 };
 var Game = /** @class */ (function () {
-    function Game(canvasEl, grid) {
+    function Game(canvasEl, grid, width, height) {
         this.canvasEl = canvasEl;
         this.grid = grid;
+        this.width = width;
+        this.height = height;
+        this.ctx = this.canvasEl.getContext("2d");
     }
-    Game.prototype.render = function () {
-        // todo does this need to happen every tick
-        // todo fps counter
-        var ctx = this.canvasEl.getContext("2d");
-        this.grid.render(ctx);
-    };
     Game.prototype.init = function () {
-        this.canvasEl.width = window.innerWidth - 50;
-        this.canvasEl.height = window.innerHeight - 20;
+        this.canvasEl.width = this.width;
+        this.canvasEl.height = this.height;
+    };
+    Game.prototype.render = function (state) {
+        this.grid.render(this.ctx, state);
     };
     return Game;
 }());
@@ -31,24 +52,55 @@ var Grid = /** @class */ (function () {
         this.width = width;
         this.height = height;
     }
-    Grid.prototype.render = function (ctx) {
+    Grid.prototype.render = function (ctx, state) {
+        this.drawBorder(ctx);
         var cellWidth = ctx.canvas.width / this.width;
-        for (var i = 0; i < this.width; i++) {
+        var cellHeight = ctx.canvas.height / this.height;
+        this.drawColumns(ctx, cellWidth);
+        this.drawRows(ctx, cellHeight);
+        this.drawCells(ctx, state, cellWidth, cellHeight);
+    };
+    Grid.prototype.drawBorder = function (ctx) {
+        ctx.moveTo(0, 0);
+        ctx.lineWidth = 4;
+        ctx.lineTo(0, ctx.canvas.height);
+        ctx.lineTo(ctx.canvas.width, ctx.canvas.height);
+        ctx.lineTo(ctx.canvas.width, 0);
+        ctx.lineTo(0, 0);
+        ctx.stroke();
+        ctx.lineWidth = 1;
+    };
+    Grid.prototype.drawColumns = function (ctx, cellWidth) {
+        for (var i = 1; i < this.width; i++) {
             var x = i * cellWidth;
-            ctx.moveTo(Math.floor(x), 0);
-            ctx.lineTo(Math.floor(x), ctx.canvas.height);
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, ctx.canvas.height);
             ctx.stroke();
         }
-        var cellHeight = ctx.canvas.height / this.height;
-        for (var i = 0; i < this.height; i++) {
+    };
+    Grid.prototype.drawRows = function (ctx, cellHeight) {
+        for (var i = 1; i < this.height; i++) {
             var y = i * cellHeight;
-            ctx.moveTo(0, Math.floor(y));
-            ctx.lineTo(ctx.canvas.width, Math.floor(y));
+            ctx.moveTo(0, y);
+            ctx.lineTo(ctx.canvas.width, y);
             ctx.stroke();
+        }
+    };
+    Grid.prototype.drawCells = function (ctx, state, cellWidth, cellHeight) {
+        for (var i = 0; i < this.width; i++) {
+            for (var j = 0; j < this.height; j++) {
+                var x = i * cellWidth;
+                var y = j * cellHeight;
+                var player = state.grid[j][i];
+                if (player) {
+                    ctx.fillStyle = "#" + state.players[player];
+                    ctx.fillRect(x, y, cellWidth, cellHeight);
+                }
+            }
         }
     };
     return Grid;
 }());
-var game = new Game(document.getElementById("game"), new Grid(40, 20));
+var game = new Game(document.getElementById("game"), new Grid(packet.grid.length, packet.grid[0].length), window.innerWidth * 0.7, window.innerHeight * 0.7);
 game.init();
-requestAnimationFrame(game.render.bind(game));
+requestAnimationFrame(game.render.bind(game, packet));
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,35 +1,67 @@
 
-const packet = {
+interface GameState {
+  players: Record<PlayerId, HexColor>,
+  grid: Cell[][]
+}
+
+type HexColor = string;
+type PlayerId = string;
+type Cell = PlayerId | null;
+
+const packet: GameState = {
   players: {
     "Bob": "ff00cc",
     "Jose": "00ffee",
   },
   grid: [
-    ["Bob", null, null, null, null, null],
-    [null, null, "Jose", null, null, null],
-    [null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
+    ["Bob", null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, "Jose", null, null, null, "Bob", null, null, null, null, null],
+    [null, null, "Jose", null, null, null, "Bob", null, null, null, null, null, null, null, null, "Jose", null, null, null, null, null, "Jose", null, null],
   ]
 };
 
 
 class Game {
 
+  private readonly ctx: CanvasRenderingContext2D;
+
   constructor(
     private readonly canvasEl: HTMLCanvasElement,
     private readonly grid: Grid,
-  ) {}
-
-  public render(): void {
-    // todo does this need to happen every tick
-    // todo fps counter
-    const ctx: CanvasRenderingContext2D = this.canvasEl.getContext("2d")!;
-
-    this.grid.render(ctx);
+    private readonly width: number,
+    private readonly height: number
+  ) {
+    this.ctx = this.canvasEl.getContext("2d")!;
   }
 
   public init(): void {
-    this.canvasEl.width = window.innerWidth - 50;
-    this.canvasEl.height = window.innerHeight - 20;
+    this.canvasEl.width = this.width;
+    this.canvasEl.height = this.height;
+  }
+
+  public render(state: GameState): void {
+    this.grid.render(this.ctx, state);
   }
 
 }
@@ -40,33 +72,69 @@ class Grid {
     private readonly height: number
   ) {}
 
-  public render(ctx: CanvasRenderingContext2D): void {
+  public render(ctx: CanvasRenderingContext2D, state: GameState): void {
+    this.drawBorder(ctx);
+
     const cellWidth = ctx.canvas.width / this.width;
-
-    for (let i = 0; i < this.width; i++) {
-      const x = i * cellWidth;
-      ctx.moveTo(Math.floor(x), 0);
-      ctx.lineTo(Math.floor(x), ctx.canvas.height);
-      ctx.stroke();
-    }
-
     const cellHeight = ctx.canvas.height / this.height;
 
-    for (let i = 0; i < this.height; i++) {
-      const y = i * cellHeight;
-      ctx.moveTo(0, Math.floor(y));
-      ctx.lineTo(ctx.canvas.width, Math.floor(y));
+    this.drawColumns(ctx, cellWidth);
+    this.drawRows(ctx, cellHeight);
+    this.drawCells(ctx, state, cellWidth, cellHeight);
+  }
+
+  private drawBorder(ctx: CanvasRenderingContext2D): void {
+    ctx.moveTo(0, 0);
+    ctx.lineWidth = 4;
+    ctx.lineTo(0, ctx.canvas.height);
+    ctx.lineTo(ctx.canvas.width, ctx.canvas.height);
+    ctx.lineTo(ctx.canvas.width, 0);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+    ctx.lineWidth = 1;
+  }
+
+  private drawColumns(ctx: CanvasRenderingContext2D, cellWidth: number): void {
+    for (let i = 1; i < this.width; i++) {
+      const x = i * cellWidth;
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, ctx.canvas.height);
       ctx.stroke();
     }
+  }
 
+  private drawRows(ctx: CanvasRenderingContext2D, cellHeight: number): void {
+    for (let i = 1; i < this.height; i++) {
+      const y = i * cellHeight;
+      ctx.moveTo(0, y);
+      ctx.lineTo(ctx.canvas.width, y);
+      ctx.stroke();
+    }
+  }
+
+  private drawCells(ctx: CanvasRenderingContext2D, state: GameState, cellWidth: number, cellHeight: number): void {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        const x = i * cellWidth;
+        const y = j * cellHeight;
+        const player = state.grid[j][i];
+
+        if (player) {
+          ctx.fillStyle = "#" + state.players[player];
+          ctx.fillRect(x, y, cellWidth, cellHeight);
+        }
+      }
+    }
   }
 }
 
 const game = new Game(
   document.getElementById("game") as HTMLCanvasElement,
-  new Grid(40, 20)
+  new Grid(packet.grid.length, packet.grid[0].length),
+  window.innerWidth * 0.7,
+  window.innerHeight * 0.7
 );
 
 game.init();
 
-requestAnimationFrame(game.render.bind(game));
+requestAnimationFrame(game.render.bind(game, packet));
